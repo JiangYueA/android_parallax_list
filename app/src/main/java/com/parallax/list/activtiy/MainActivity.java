74d0bc94808da942d012023697750f5e89fcaede
姜岳 <jiangyue@bestapp.us>
2016-04-27 14:58:14 +0800

    put init activity

---
+++ app/src/main/java/com/parallax/list/activtiy/MainActivity.java
@@ -0,0 +1,25 @@
+package com.parallax.list.activtiy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.parallax.list.R;
+
+/**
+ * Created by jiangyue on 16/4/27.
+ */
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
